b984f852dcc73c00d281234ec2b2c5e6933dc042
Amogh-4 <jaasir.subair@gmail.com>
2026-01-09 12:48:47 +0530

    ridge regression alpha=0.5 split=0.2'

---
+++ train.py
@@ -0,0 +1,47 @@
+import os
+import json
+import pickle
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error, r2_score
+
+print("Mohamed Jaasir Subair")
+print("2022BCS0010")
+
+os.makedirs("output/model", exist_ok=True)
+os.makedirs("output/results", exist_ok=True)
+
+df = pd.read_csv("dataset/winequality-red.csv", sep=";")
+
+X = df.drop("quality", axis=1)
+y = df["quality"]
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.20, random_state=42
+)
+
+model = Ridge(alpha=0.5, fit_intercept=True)
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, pred)
+r2 = r2_score(y_test, pred)
+
+print("MSE:", mse)
+print("R2:", r2)
+
+
+with open("output/model/trained_model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+metrics = {
+    "MSE": mse,
+    "R2": r2
+}
+
+with open("output/results/metrics.json", "w") as f:
+    json.dump(metrics, f, indent=4)
