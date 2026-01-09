ea5d376cd4793e35ccef783366304b969dddcd34
Amogh-4 <jaasir.subair@gmail.com>
2026-01-09 15:47:14 +0530

    Random forest n_estimator=100 max_depth=10

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ y = df["quality"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
-
+''
 model = RandomForestRegressor(
     n_estimators=100, max_depth=10, random_state=42)
 
