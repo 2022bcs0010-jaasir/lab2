ac08026071f3a08a856872fce89cee43b29701cd
Amogh-4 <jaasir.subair@gmail.com>
2026-01-09 15:37:34 +0530

    max depth=6 min smaples leaf=5

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ import json
 import pickle
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler
+
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.tree import DecisionTreeRegressor
 
 print("Mohamed Jaasir Subair")
 print("2022BCS0010")
@@ -20,29 +20,29 @@ X = df.drop("quality", axis=1)
 y = df["quality"]
 
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
 X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.25, random_state=42)
+    X, y, test_size=0.2, random_state=42)
 
-model = Ridge(alpha=0.7, fit_intercept=True)
+model = DecisionTreeRegressor(
+    max_depth=6, min_samples_leaf=5, random_state=42)
+
 model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
 
-mse_exp02 = mean_squared_error(y_test, pred)
-r2_exp02 = r2_score(y_test, pred)
+mse_exp03 = mean_squared_error(y_test, pred)
+r2_exp03 = r2_score(y_test, pred)
 
-print("LR-02 (Standardized) MSE:", mse_exp02)
-print("LR-02 (Standardized) R2 :", r2_exp02)
+print("DT-01 MSE:", mse_exp03)
+print("DT-01 R2 :", r2_exp03)
+
 
 with open("output/model/trained_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
 metrics = {
-    "MSE": mse_exp02,
-    "R2": r2_exp02
+    "MSE": mse_exp03,
+    "R2": r2_exp03
 }
 
 with open("output/results/metrics.json", "w") as f:
