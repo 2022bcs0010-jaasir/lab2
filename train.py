8afab239c8f8fb8e2de3a019e892825cbd11507e
Amogh-4 <jaasir.subair@gmail.com>
2026-01-09 15:23:37 +0530

    Exp 2Ridge regrssion with correlation based feature selection

--- train.py
+++ train.py
@@ -19,8 +19,15 @@ X = df.drop("quality", axis=1)
 y = df["quality"]
 
 
+corr = X.corrwith(y).abs().sort_values(ascending=False)
+top5_features = corr.index[:5]
+
+print("Selected Features:", list(top5_features))
+
+X_selected = X[top5_features]
+
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.20, random_state=42
+    X_selected, y, test_size=0.20, random_state=42
 )
 
 model = Ridge(alpha=0.5, fit_intercept=True)
@@ -28,19 +35,18 @@ model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
 
-mse = mean_squared_error(y_test, pred)
-r2 = r2_score(y_test, pred)
+mse_exp01_fs = mean_squared_error(y_test, pred)
+r2_exp01_fs = r2_score(y_test, pred)
 
-print("MSE:", mse)
-print("R2:", r2)
-
+print("LR-01 (Feature Selected) MSE:", mse_exp01_fs)
+print("LR-01 (Feature Selected) R2 :", r2_exp01_fs)
 
 with open("output/model/trained_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
 metrics = {
-    "MSE": mse,
-    "R2": r2
+    "MSE": mse_exp01_fs,
+    "R2": r2_exp01_fs
 }
 
 with open("output/results/metrics.json", "w") as f:
